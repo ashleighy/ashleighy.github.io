a9fcda3e00e0dddb72f98a142de73bf3cc269edb
Ginger <ginger.eyelashes@gmail.com>
2015-09-24 17:18:43 +0200

    wip page

---
+++ js/fadein.js
@@ -0,0 +1,69 @@
+
+$(document).ready(function(){
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 20) {
+                  $(".container").addClass("fadeInUp");
+                  $(".footer").fadeIn(1500);
+
+            } else {
+              $(".footer").fadeOut(1500);
+              $(".container").removeClass("fadeInUp");
+
+            }
+
+            if ($(this).scrollTop() > 600){
+                                $(".buttonsbottom").addClass("scrolled");
+                  $(".buttonss").css("background-color","#FCDDDE");
+            }
+              else{
+                              $(".buttonsbottom").removeClass("scrolled");
+              $(".buttonss").css("background-color","transparent");
+              }
+
+            if ($(this).scrollTop() >900){
+              $(".port").addClass("animate");
+              $(".port").css("opacity","1");
+            }
+        });
+    });
+
+
+// $('.port').on('click', function() {
+//   $(this).toggleClass("selected");
+//     $(".main2").slideDown(1500);
+//    $(".main").slideUp("slow");
+// });
+
+// $(".exit").on('click', function() {
+//  $("html, body").animate({ scrollTop: 0 }, "slow");
+// location.reload();
+// });
+
+// $(".fa.fa-user.fa-2x").click(function(){
+//   $(".container").slideDown(1500);
+//   $(".aboutcontainer").slideDown(1500);
+//   $(".portcontainer, .contactcontainer").slideUp();
+//   $(".exit").css("display","block");
+// });
+
+// $(".fa.fa-th.fa-2x").click(function(){
+//   $(".container").slideDown(1500);
+//   $(".portcontainer ").slideDown(1500);
+//    $(".aboutcontainer, .contactcontainer").slideUp();
+//   $(".exit").css("display","block");
+// });
+
+
+
+// var clicked;
+
+// $('.port').click(function(){
+//     clicked = $(this);
+// });
+
+//$(clicked).on('click', function() {
+//  $(".main2").slideDown(1500);
+//  $(".main").slideUp("slow");
+//});
+
+
